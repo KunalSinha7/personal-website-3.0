17ba086c48cb22adcb75096da917105492a97d7a
Kunal Sinha <kunalsinha98@gmail.com>
2019-01-09 16:34:13 -0500

    Removed Insta, Added Resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import resume from '../Kunal_Sinha.pdf'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <ul className="icons">
                 <li><a href="https://twitter.com/sinhaRMA" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                 <li><a href="https://www.facebook.com/kunal.sinha.355" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+                <li><a href={resume} target="_blank" className="icon fa-file"><span className="label">Resume</span></a></li>
                 <li><a href="https://github.com/KunalSinha7/" className="icon fa-github"><span className="label">GitHub</span></a></li>
         </ul>        
     </footer>
