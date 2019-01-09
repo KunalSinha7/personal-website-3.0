7222ace3c87c1ae192cd9d04b918b3aba863bf1f
Kunal Sinha <kunalsinha98@gmail.com>
2019-01-09 01:33:52 -0500

    Edited gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Kunal Sinha",
+    author: "Kunal Sinha",
+    description: "Kunal Sinha's Personal Website"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Kunal Sinha Profile',
+        short_name: 'Kunal Sinha Profile',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#0d182e',
+        theme_color: '#0d182e',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon2.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
